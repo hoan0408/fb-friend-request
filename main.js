5241b19b49b3d5202d00d63a83b646ab670c0a61
hoan0408 <161062118+hoan0408@users.noreply.github.com>
2025-02-12 19:05:18 +0700

    main.js

--- main.js
+++ main.js
@@ -1,16 +1,48 @@
 const Btnconfirm = document.querySelectorAll(".confirm")
-Btnconfirm.forEach(button =>{
-    button.addEventListener("click",()=>
-    {
-        // button.innerText
-        button.textContent = 'Accepted'; 
+Btnconfirm.forEach(button => {
+    button.addEventListener("click", () => {
+
+        //yeu cau 1 2
+        // button.innerText 
+        button.textContent = 'Accepted';
         button.classList.add("clicked");
+        // yeu cau 3
         const box = button.parentElement;
         // box.parentElement.removeChild(box);
         box.remove();
     })
 }
 )
+
+
+const Btnnextright = document.querySelector(".next-right")
+const Btnnextleft = document.querySelector(".next-left")
+const boxes = document.querySelector(".boxs")
+
+
+let currentindex = 1;
+
+let currentindexright = 1;
+let currentindexleft = 1;
+const movemount = 20;
+
+Btnnextright.addEventListener('click', () => {
+    Btnnextleft.classList.add("open-left");
+    const newTransformValue = -movemount * currentindexright;
+    boxes.style.transform = `translateX(${newTransformValue}%)`
+    currentindexright++;
+    currentindexleft = 0;
+})
+
+Btnnextleft.addEventListener('click', () => {
+    const newTransformValue = movemount * currentindexleft;
+    boxes.style.transform = `translateX(${newTransformValue}%)`
+    currentindexleft++;
+    currentindexright = 0;
+})
+
+
+
 
 
 
